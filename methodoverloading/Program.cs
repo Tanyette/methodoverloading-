409b09ed5ba753a709879467d77b616281e1655a
Tanyette Wiggison <bwaffiliates@yahoo.com>
2022-01-19 10:27:55 -0700

    first commit

---
+++ methodoverloading/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace methodoverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
